cb8ef271710e72767978ed191790b2c907be050e
Ryan Rottmann <ryanrottmann@Ryans-MacBook-Pro.local>
2019-02-01 21:21:54 -0600

    Fixed app

---
+++ Expenses Challenge Solution/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses Challenge Solution
+//
+//  Created by Ryan Rottmann on 1/28/19.
+//  Copyright © 2019 Ryan Rottmann. All rights reserved.
+//
+
+import Foundation
+
+struct Expense{
+    let title: String
+    let amount: Double
+    let date: Date
+}
